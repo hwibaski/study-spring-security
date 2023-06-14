c976b86206a08a3405b080e1fb89f6c3ad81147c
ralph <hwibaski@gmail.com>
2023-06-14 17:29:02 +0900

    initial commit

---
+++ src/main/java/me/hwibaski/securityinaction/SecurityinactionApplication.java
@@ -0,0 +1,13 @@
+package me.hwibaski.securityinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityinactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityinactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hwibaski/securityinaction/SecurityinactionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwibaski.securityinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityinactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
