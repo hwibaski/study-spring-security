42c8ddc5128cfce83d7ce80af6d0f5f343eede79
ralph <hwibaski@gmail.com>
2023-06-14 18:18:29 +0900

    UserDetailsService 구성 요소 재정의

---
+++ src/main/java/me/hwibaski/securityinaction/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package me.hwibaski.securityinaction.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("hwibaski").password("12345").authorities("read").build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
