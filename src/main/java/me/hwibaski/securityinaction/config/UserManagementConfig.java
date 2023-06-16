f866c85c71ffbed0087e42260285e29b7249e646
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-06-16 15:37:46 +0900

    UserDetails, UserDetailsService 구현 (#4)
    * UserDetails 인터페이스 구현한 SecurityUser
    
    * UserDetailsService 인터페이스 구현한 커스텀 service

--- src/main/java/me/hwibaski/securityinaction/config/UserManagementConfig.java
+++ src/main/java/me/hwibaski/securityinaction/config/UserManagementConfig.java
@@ -1,5 +1,7 @@
 package me.hwibaski.securityinaction.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
@@ -9,6 +11,10 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import me.hwibaski.securityinaction.model.JpaUser;
+import me.hwibaski.securityinaction.model.SecurityUser;
+import me.hwibaski.securityinaction.service.InMemoryUserDetailsService;
+
 /**
  * AuthenticationProvider를 구현한 구현체가 있으면 아래의 설정이 적용되지 않는다.
  */
@@ -16,9 +22,11 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class UserManagementConfig {
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withUsername("hwibaski").password("12345").authorities("read").build();
+        JpaUser jpaUser = new JpaUser("hwibaski", "12345", "read");
+        UserDetails userDetails = new SecurityUser(jpaUser);
 
-        return new InMemoryUserDetailsManager(userDetails);
+        List<UserDetails> users= List.of(userDetails);
+        return new InMemoryUserDetailsService(users);
     }
 
     @Bean
